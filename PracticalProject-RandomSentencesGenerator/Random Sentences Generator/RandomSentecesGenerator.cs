aa9770d6cf64e98c595d129dad533f3de8a3a2db
Grigor Jivkov Marinov <114864279+GrigorMarinov@users.noreply.github.com>
2023-02-20 14:08:18 +0200

    new task

---
+++ PracticalProject-RandomSentencesGenerator/Random Sentences Generator/RandomSentecesGenerator.cs
@@ -0,0 +1,35 @@
+﻿string[] names = { "Peter", "Michell", "Jane", "Steve" };
+string[] places = { "Sofia", "Plovdiv", "Varna", "Burgas"};
+string[] verbs = { "eats", "holds", "sees", "plays with", "brings" };
+string[] nouns = { "stones", "cake", "apple", "laptop", "bikes"};
+string[] adverbs = { "slowly", "diligently", "warmly", "sadly", "rapidly"};
+string[] details = { "near the river", "at home", "in the park"};
+
+
+while (true)
+{
+    string randomName = GetRandomWord(names);
+    string randomPlace = GetRandomWord(places);
+    string randomVerb = GetRandomWord(verbs);
+    string randomNouns = GetRandomWord(nouns);
+    string randomAdverbs = GetRandomWord(adverbs);
+    string randomDetails = GetRandomWord(details);
+
+    string who = $"{randomName} from {randomPlace}";
+    string action = $"{randomAdverbs} {randomVerb} {randomNouns}";
+    string sentences = $"{who} {action} {randomDetails}";
+
+    Console.WriteLine(sentences);
+    Console.WriteLine("Click [Enter] to generate a new one.");
+
+    Console.ReadLine();
+}
+
+string GetRandomWord(string[] words)
+{
+    Random random = new Random();
+    int randomIndex = random.Next(words.Length);
+    string word = words[randomIndex];
+
+    return word;
+}
